22eb9ff9ac54d2fa7742022aa051b5a79532a338
Johanna Bromark <johanna.bromark@gmail.com>
2018-01-23 14:33:45 +0100

    Small change to heuristic.py

--- heuristic.py
+++ heuristic.py
@@ -1,2 +1,3 @@
 def heuristic(nodeFrom, nodeTo):
+    """Takes two nodes and computes the distance between them"""
     return  math.sqrt(math.exp(nodeFrom.x - nodeTo.x) + math.exp(nodeFrom.y-nodeTo.y))
