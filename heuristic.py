60390201fcf7000a2089cbafec09ad749ccc5877
Johanna Bromark <johanna.bromark@gmail.com>
2018-01-23 14:44:05 +0100

    Added function generateNeghbors

--- heuristic.py
+++ heuristic.py
@@ -1,3 +1,15 @@
 def heuristic(nodeFrom, nodeTo):
     """Takes two nodes and computes the distance between them"""
     return  math.sqrt(math.exp(nodeFrom.x - nodeTo.x) + math.exp(nodeFrom.y-nodeTo.y))
+
+def generateNeighbors(current):
+    """Takes a node and generates its neighbors as nodes"""
+    neighbors = []
+    x = current.x
+    y = current.y
+    neighbors.append(Node(x, y+1))
+    neighbors.append(Node(x, y-1))
+    neighbors.append(Node(x+1, y))
+    neighbors.append(Node(x-1), y)
+
+    return neighbors
