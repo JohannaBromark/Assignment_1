f6bde7b948c8af25760ef0b160c692e8fd74f6e6
Johanna <johanna.bromark@gmail.com>
2018-02-01 12:30:45 +0100

    Changed obstacle read

--- Map.py
+++ Map.py
@@ -5,28 +5,22 @@ from obstacleCheck import *
 class Map():
 
     def __init__(self, file):
-        self.width = 10
-        self.height = 10
+        self.width = 60
+        self.height = 60
         
         data = json.load(open(file))
 
         self.bounding_polygon = np.array(data["bounding_polygon"])
 
-        #print(self.bounding_polygon)
         self.obstacles = []
-        try:
-            i = 0;
-            while(1):
-                self.obstacles.append(np.array((data["obstacle_"+str(i+1)])))
-                i+=1
-        except KeyError:
-            pass
+        for key in data:
+            if "obstacle_" in key:
+                self.obstacles.append(np.array((data[key])))
 
         #Hitboxes for obstacles for an easy test if a node is outside an obstacle and inside the bounding_polygon
         self.hitBoxes = makeBoxes(self.obstacles)
         self.boundingHitBox = [findMaxMin(self.bounding_polygon)]
 
-        #print(self.obstacles[1])
         self.pos_goal = data["pos_goal"]
         self.pos_start = data["pos_start"]
         self.vehicle_L = data["vehicle_L"]
